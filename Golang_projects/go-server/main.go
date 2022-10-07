1c4f8765b9c2229dfe078d4e26e772698cb913ed
yaksh1 <yakshgandhi1@gmail.com>
2022-10-08 03:01:21 +0530

    updating

--- Golang_projects/go-server/main.go
+++ Golang_projects/go-server/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8") // to set the content type of body
+	fmt.Fprint(w, "<h2>Welcome to my website testing!</h2>")
+}
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
@@ -19,25 +23,42 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprint(w, "<h1>Contact Page</h1>  <p>To get in touch email me at <a href = \"mailto:yakshgandhi1@gmail.com\">yakshgandhi1@gmail.com</a></p>") // contact page
+
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	// if r.URL.Path == "/contact" {
+	// 	contactHandler(w, r)
+	// } else if r.URL.Path == "/form" {
+	// 	formHandler(w, r)
+	// } else if r.URL.Path == "/" {
+	// 	homeHandler(w, r)
+	// }
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/form":
+		formHandler(w, r)
+	default:
+		// TODO: here we will handle the page not found error
 		http.Error(w, "404 page not found", http.StatusNotFound)
-		return
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprint(w, "404 page not found")
 	}
-	if r.Method != "GET" {
-		http.Error(w, "method is not supported", http.StatusNotFound)
-	}
-	fmt.Fprintf(w, "hello!")
+
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
-
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server at port :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	//setting the localhost
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	log.Fatal(err)
+	// }
+	http.ListenAndServe(":8080", nil)
 }
